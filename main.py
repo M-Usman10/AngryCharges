e42a9027ae2b442a30c3822bf21626be16b7ca94
physxP <48580461+physxP@users.noreply.github.com>
2019-07-27 21:37:02 +0500

    first position estimator

--- main.py
+++ main.py
@@ -2,23 +2,27 @@
 import pygame
 from pygame.locals import *
 from background import displayBackground
+from q_position import position_estimator
 # 2 - Initialize the game
 pygame.init()
-width, height = 800, 650
+width, height = 1920, 1080
 screen=pygame.display.set_mode((width, height))
 
 # 3 - Load images
 box1 = pygame.image.load("resources/1.jpg")
 box2 = pygame.image.load("resources/2.jpg")
 
-
+p = ([0, 0, 0], [100, 0, 0])
 # 4 - keep looping through
 while 1:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the screen elements
-    displayBackground(screen,box1,box2,width,height)
+    # displayBackground(screen,box1,box2,width,height)
     # 7 - update the screen
+    p = position_estimator([10, 0, 0], [0, 0, 1], 1, p[0], p[1], 1, 0.01)
+    pygame.draw.circle(screen,[255,0,0],[int(p[0][0]+1920/2),int(p[0][1]+1080/2)],10)
+
     pygame.display.flip()
     # 8 - loop through the events
     for event in pygame.event.get():
--- q_position.py
+++ q_position.py
@@ -4,28 +4,31 @@ import matplotlib.pyplot as plt
 
 
 def force(E, B, q, r, v):
-    return q * (E + np.cross(v, B))
+    return q * (np.array(E) + np.cross(v, B))
 
 
 def position_estimator(E, B, q, r, v, m, dt):
     F = force(E, B, q, r, v)
     vn = F * dt / m + v
     rn = vn * dt + r
-    return (rn, vn)
+    return np.array([rn, vn])
 
 
 def test_positon_estimator():
-    t=100
-    n = 10000
+    t=1000
+    n = 100000
     pos = np.zeros([n, 2, 3])
-    p = ([0, 0, 0], [.1, 0, 0])
+    p = ([0, 0, 0], [0, 0.1, 0])
 
     for i in range(0, n):
         pos[i] = p
-        p = position_estimator([0, 0, 0], [0, 0, 1], 1, p[0], p[1], 1, t/n)
+        p = position_estimator([0, 1, 0], [0, 0,0.01 ], 1, p[0], p[1], 1, t/n)
+        # if i%1 == 0:
+        #     plt.scatter(p[0][0],p[0][1])
+
     plt.figure()
     plt.plot(pos[:, 0, 0], pos[:, 0, 1])
     plt.title('delta t = ' + str(t/n))
     plt.show()
 
-test_positon_estimator()
+#test_positon_estimator()
